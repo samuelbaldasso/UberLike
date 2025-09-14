154786c58cd448389a2a99ed85429d6d90f5f0ea
Samuel Baldasso <samuelbaldasso93@gmail.com>
2025-09-14 14:24:56 -0300

    Revert "feat: add PostgreSQL configuration and update dependencies; implement tests for user, delivery, location, and notification modules"
    This reverts commit b149a5840da3958641b7f72c34086df60bf716c7.

--- src/test/java/com/sbaldasso/combobackend/CombobackendApplicationTests.java
+++ src/test/java/com/sbaldasso/combobackend/CombobackendApplicationTests.java
@@ -2,12 +2,14 @@ package com.sbaldasso.combobackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class CombobackendApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+	@Test
+	void contextLoads() {
+	}
 
 }
